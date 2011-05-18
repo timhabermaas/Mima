b7d4ddad077f7e0272e22842e87c4355467139bb
Tim Habermaas <tim.habermaas@gmail.com>
2011-05-18 23:02:00 +0200

    added argument to main file

--- mima.rb
+++ mima.rb
@@ -2,8 +2,12 @@ require './lib/tokenizer'
 require './lib/mima'
 require './init.rb'
 
-tokenizer = Tokenizer.new(File.open("test.mima"))
-mima = Mima.new(tokenizer)
-mima.run
+if ARGV[0]
+  tokenizer = Tokenizer.new(File.open(ARGV[0]))
+  mima = Mima.new(tokenizer)
+  mima.run
 
-puts "Akku: 0x#{mima.akku.to_s(16)}"
+  puts "Akku: 0x#{mima.akku.to_s(16)}"
+else
+  puts "Usage: ruby mima.rb <file>"
+end
